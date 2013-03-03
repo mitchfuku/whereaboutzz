340f4465aea6eeba6921e85271fd0091f785c186
Mitchell Fukumoto <mitchsf777@gmail.com>
2013-03-03 02:17:13 -0800

    added map functionality

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
 
 @app.route('/')
+def getLocation():
+    return render_template('get-location.html')
+# def home():
+#     return render_template('home.html', 
+#     	homeActive = 'current_page_item')
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+    lat = request.form['lat']
+    lon = request.form['lon']
     return render_template('home.html', 
-    	homeActive = 'current_page_item')
+        homeActive = 'current_page_item',
+        lat = lat,
+        lon = lon)
 
 @app.route('/nearby')
 def nearby():
@@ -26,4 +37,5 @@ def photoTour():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
