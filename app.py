51fe34754d64711ff6dd50539e730d8721bc1efb
Mitchell Fukumoto <mitchsf777@gmail.com>
2013-03-16 15:45:05 -0700

    added food display on nearby page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ sys.path.insert(0, "python")
 
 foursquare_client_id = "FBZHIPP1BP5UYHHDOBS1LM3E5HQS1WUFCY4XFI5WZOE45TPD"
 foursquare_client_secret = "WIAOI4PMNQUYNSQR4XGDYULWU3WTBFMC2XHMVZ4N1FXIZNOF"
+# This is in meters per hour
+walking_speed = 5000
+# This is in hours
+tour_length = 1
 
 def sessionCheck():
     if 'lat' in session and 'locality' in session:
@@ -17,15 +21,40 @@ def getFoursquareVenuesNearby(lat, lon):
     # https://api.foursquare.com/v2/venues/search?query=ise%20sushi&ll=40.7143528%2C-74.00597309999999
     latlon = urllib.quote(str(lat) + "," + str(lon))
     opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')] #wikipedia needs this
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    radius = (tour_length * walking_speed) / 2 # they have to walk back too
 
-    url = "https://api.foursquare.com/v2/venues/explore?v=20130315&client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret + "&intent=browse&ll=" + latlon
-    resource = opener.open(url)
-    data = resource.read()
+    url = "https://api.foursquare.com/v2/venues/explore?v=20130315&venuePhotos=1&client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret + "&intent=browse&ll=" + latlon + "&radius=" + str(radius)
+    urlFood = url + "&section=food"
+    urlSights = url + "&section=sights&limit=10"
+    urlOutdoors = url + "&section=outdoors&limit=10"
+    urlArts = url + "&section=arts&limit=10"
+    resource = opener.open(urlFood)
+    dataFood = resource.read()
     resource.close()
-    data = str(data).decode('utf8')
+    resource = opener.open(urlSights)
+    dataSights = resource.read()
+    resource.close()
+    resource = opener.open(urlOutdoors)
+    dataOutdoors = resource.read()
+    resource.close()
+    resource = opener.open(urlArts)
+    dataArts = resource.read()
+    resource.close()
+    dataFood = str(dataFood).decode('utf8')
+    dataSights = str(dataSights).decode('utf8')
+    dataOutdoors = str(dataOutdoors).decode('utf8')
+    dataArts = str(dataArts).decode('utf8')
     print url
     sys.stdout.flush();
+
+    data = json.dumps(
+        dict(
+            food=json.loads(dataFood),
+            sights=json.loads(dataSights),
+            outdoors=json.loads(dataOutdoors),
+            arts=json.loads(dataArts))
+        )
 
     return json.loads(data)
 
@@ -111,40 +140,48 @@ def getLocation():
     else:
         return render_template('get-location.html')
 
-@app.route('/set-location', methods=['GET', 'POST'])
+@app.route('/user-location', methods=['GET', 'POST'])
 def storeLocation():
     from flask import make_response, redirect, url_for
     import time
-    response = make_response(redirect('/'))
-    if 'redirect' in request.form:
-        response = make_response(redirect(request.form['redirect']))
-    lat = request.form['lat']
-    lon = request.form['lon']
-    #reverse geocode lat and lon
-    types = ['locality', 'administrative_area_level_1']
-    for geoname in get_geonames(lat, lon, types):
-        # set name of city and state in a session
-        if 'locality' in geoname['types']:
-            session['locality'] = geoname['long_name']
-        elif 'administrative_area_level_1' in geoname['types']:
-            session['city_long'] = geoname['long_name']
-            session['city_short'] = geoname['short_name']
+    if request.method == 'POST':
+        response = make_response(redirect('/'))
+        if 'redirect' in request.form:
+            response = make_response(redirect(request.form['redirect']))
+        lat = request.form['lat']
+        lon = request.form['lon']
+        #reverse geocode lat and lon
+        types = ['locality', 'administrative_area_level_1']
+        for geoname in get_geonames(lat, lon, types):
+            # set name of city and state in a session
+            if 'locality' in geoname['types']:
+                session['locality'] = geoname['long_name']
+            elif 'administrative_area_level_1' in geoname['types']:
+                session['city_long'] = geoname['long_name']
+                session['city_short'] = geoname['short_name']
 
-    session['lat'] = lat
-    session['lon'] = lon
-    return response
+        session['lat'] = lat
+        session['lon'] = lon
+        return response
+    if request.method == 'GET':
+        return "to implement"
 
 @app.route('/nearby')
 def nearby():
     if sessionCheck():
         lat = float(escape(session['lat']))
         lon = float(escape(session['lon']))
+        city = str(escape(session['locality']))
+        state_long = str(escape(session['city_long']))
+        state_short = str(escape(session['city_short']))
         data = getFoursquareVenuesNearby(lat, lon)
-        print data
         return render_template('nearby.html',
             data = data,
             lat = lat,
             lon = lon,
+            city = city,
+            state_long = state_long,
+            state_short = state_short,
             nearbyActive = 'current_page_item')
     else:
         return render_template('get-location.html',
